1a352390e84007f1f1cc3f3357cbbbfe97e4f37c
Xiao Yao <yaoxiao89@gmail.com>
2018-03-17 18:30:33 -0400

    Setup project

---
+++ XYCoreData/XYCoreData.h
@@ -0,0 +1,19 @@
+//
+//  XYCoreData.h
+//  XYCoreData
+//
+//  Created by Xiao Yao on 3/17/18.
+//  Copyright © 2018 Xiao Yao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for XYCoreData.
+FOUNDATION_EXPORT double XYCoreDataVersionNumber;
+
+//! Project version string for XYCoreData.
+FOUNDATION_EXPORT const unsigned char XYCoreDataVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XYCoreData/PublicHeader.h>
+
+
