9200d4a15fe4de2073aedfabce82002b43de1a31
Xiao Yao <yaoxiao89@gmail.com>
2018-06-03 20:47:09 -0400

    Import CoreDataController implementation

---
+++ XYCoreData/CoreDataController.swift
@@ -0,0 +1,92 @@
+//
+//  CoreDataController.swift
+//  XYCoreData
+//
+//  Copyright © 2018 Xiao Yao. All rights reserved.
+//  See LICENSE.txt for licensing information
+//
+
+import CoreData
+import XYCommon
+import XYUserDefaults
+
+// MARK: - CoreDataControllerObserver
+
+@objc public protocol CoreDataControllerObserver: AnyObject {
+    
+    func refreshData()
+    
+}
+
+// MARK: - CoreDataController
+
+public class CoreDataController {
+    
+    public let persistentContainer: NSPersistentContainer
+    
+    private let userDefaults: UserDefaults?
+    
+    private var notificationToken: NotificationToken?
+    
+    private let observers = NSHashTable<CoreDataControllerObserver>.weakObjects()
+    
+    public var managedObjectContext: NSManagedObjectContext {
+        return persistentContainer.viewContext
+    }
+    
+    public var shouldRefreshData: Bool {
+        guard let lastBundleID = userDefaults?.lastBundleIDToPersist else { return false }
+        return lastBundleID != Bundle.main.bundleIdentifier
+    }
+    
+    public init(groupID: String?, name: String = "Database", completionHandler: @escaping (_ error: Error?) -> Void) {
+        userDefaults = UserDefaults(suiteName: groupID)
+        
+        persistentContainer = NSPersistentContainer(
+            name: name,
+            groupID: groupID,
+            completionHandler: completionHandler
+        )
+        
+        let center = NotificationCenter.default
+        notificationToken = center.addObserver(forName: Notification.Name.NSManagedObjectContextDidSave) { [weak self] (note) in
+            guard let `self` = self else { return }
+            self.userDefaults?.lastBundleIDToPersist = Bundle.main.bundleIdentifier
+        }
+        
+        persistentContainer.viewContext.automaticallyMergesChangesFromParent = true
+    }
+    
+    public func refreshData() {
+        observers.allObjects.forEach({ $0.refreshData() })
+        userDefaults?.lastBundleIDToPersist = nil
+    }
+    
+    public func addObserver(_ observer: CoreDataControllerObserver) {
+        observers.add(observer)
+    }
+    
+}
+
+// MARK: - UserDefaults
+
+extension UserDefaults {
+    
+    private struct CoreDataControllerKeySuffixes {
+        static let lastBundleIDToPersist = "lastBundleIDToPersist"
+    }
+    
+    var lastBundleIDToPersist: String? {
+        get {
+            return value(forKey: makeKey(for: CoreDataControllerKeySuffixes.lastBundleIDToPersist)) as? String
+        }
+        set {
+            set(newValue, forKey: makeKey(for: CoreDataControllerKeySuffixes.lastBundleIDToPersist))
+        }
+    }
+    
+    func makeKey(for suffix: String) -> String {
+        return "\(UserDefaults.keyPrefix).\(suffix)"
+    }
+    
+}
